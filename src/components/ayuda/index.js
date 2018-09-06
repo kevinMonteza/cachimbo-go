3105c50a3dd0b6ce6fc1c511f383dff693b4687d
kevinMonteza <kmonteza95@gmail.com>
2018-09-06 14:59:50 -0500

    cambios css

--- src/components/ayuda/index.js
+++ src/components/ayuda/index.js
@@ -16,17 +16,26 @@ class registrar extends Component {
     render() {
         return (
             <Container>
-                <Row>
-                    <Col sm={8}>
+                <Row className="justify-content-center">
+                    <Col sm={6}>
                         <p>
-                            ¿QUÉ ES MI PERFIL? Aquí se puede visualizar la información del usuario.
-                            ¿CÓMO RESOLVER PREGUNTAS? Primero se debe ir a Resolver Test, luego seleccionamos la asignatura, luego el tema y finalmente el subtema. Haciendo esto podremos resolver las preguntas de acuerdo al subtema elegido.
-                            ¿QUÉ ES UNA RONDA? Es una colección de preguntas, para finalizar una ronda se deben responder todas las preguntas correctamente. Al finalizar una ronda se muestran los resultados correspondientes.
-                            ¿RONDA NORMAL? Esta ronda cuenta con 7 preguntas.
-                            ¿RONDA DE REPASO? Esta ronda cuenta con 10 preguntas.
-                            ¿CÓMO GANAR MONEDAS? Debe finalizar una ronda completa para ganar monedas, 2 monedas por ronda normal y 3 por ronda de repaso.
-                            ¿QUÉ ES EL HISTORIAL? Son los resultados que obtuvo en el subtema la última vez que lo resolvió.
-                             ¿QUÉ ES LA TIENDA? Una vez que haya reunido monedas suficientes, puede comprar asignaturas nuevas en la tienda.
+                            <p>¿QUÉ ES MI PERFIL?</p>
+                            <p>Aquí se puede visualizar la información del usuario.</p>
+                            <p>¿CÓMO RESOLVER PREGUNTAS?</p>
+                             <p>Primero se debe ir a Resolver Test, luego seleccionamos la asignatura, luego el tema y finalmente el subtema.
+                                Haciendo esto podremos resolver las preguntas de acuerdo al subtema elegido.</p>
+                            <p>¿QUÉ ES UNA RONDA?</p> 
+                            <p>Es una colección de preguntas, para finalizar una ronda se deben responder todas las preguntas correctamente. Al finalizar una ronda se muestran los resultados correspondientes.</p>
+                            <p>¿RONDA NORMAL?</p>
+                            <p>Esta ronda cuenta con 7 preguntas.</p>
+                            <p>¿RONDA DE REPASO?</p>
+                            <p>Esta ronda cuenta con 10 preguntas.</p>
+                            <p>¿CÓMO GANAR MONEDAS?</p>
+                            <p>Debe finalizar una ronda completa para ganar monedas, 2 monedas por ronda normal y 3 por ronda de repaso.</p>
+                            <p>¿QUÉ ES EL HISTORIAL?</p>
+                            <p>Son los resultados que obtuvo en el subtema la última vez que lo resolvió.</p> 
+                            <p>¿QUÉ ES LA TIENDA?</p> 
+                            <p>Una vez que haya reunido monedas suficientes, puede comprar asignaturas nuevas en la tienda.</p>
 
                         </p>
                     </Col>
--- src/components/perfil/index.js
+++ src/components/perfil/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Row, Col 
+    CardTitle, CardSubtitle, Row, Col, Container
 } from 'reactstrap';
-import "./styles.css";
 import profile from '../../img/image.png';
 import { FaCoins } from "react-icons/fa";
 
@@ -16,25 +15,25 @@ const perfil = () => {
     const usuario = JSON.parse(sessionStorage.getItem('user'));
     if (usuario) {
         return (
-            <div>
-                <Row>
-                    <Col>
-                        <Card className="Card" >
-                            <CardImg className="CardImg" height="400" src={profile} alt="Card image cap" />
-                            <CardBody>
-                                <CardTitle>{usuario.nombres + "  " + usuario.apellidos}</CardTitle>
+            <Container>
+                <Row className="align-items-center mx-md-5 my-md-5">
+                    <Col sm='6' className="justify-content-center">
+                        <Card>
+                            <CardImg className="CardImg" height='200px' width='100px' src={profile} alt="Card image cap" />
+                            <CardBody className="text-center">
+                                <CardTitle className="text-capitalize">{usuario.nombres + "  " + usuario.apellidos}</CardTitle>
                                 <CardSubtitle>{usuario.correo}</CardSubtitle>
-                                <CardText>{usuario.monedas} <FaCoins /></CardText>
+                                <CardText className="font-italic">{usuario.monedas} <FaCoins /></CardText>
                             </CardBody>
                         </Card>
                     </Col>
                 </Row>
+            </Container>
 
-            </div>
         );
     } else {
-        return(
-            <Redirect to="/login"/>
+        return (
+            <Redirect to="/login" />
         )
     }
 
